2c7c97ba8dc984c4a9c365263d79b88f625524d8
laitadzerati14@gmail.com <laitadzerati14@gmail.com>
2021-02-16 23:58:06 +0400

    initial

---
+++ No Sense Task/No Sense Task/ExtensionMethod.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace No_Sense_Task
+{
+    public static class ExtensionMethod
+    {
+        public static IEnumerable<T> ThisDoesntMakeAnySense<T>( this  IEnumerable<T> ts,
+            Func<T, bool> Filter,
+            Func<IEnumerable<T>> New)
+        {
+            foreach (var item in ts)
+            {
+                if (item == null)
+                    throw new ArgumentNullException();
+                if (Filter(item))
+                    return ts ;
+            }
+            return New();
+        }
+
+    }
+}
---
+++ No Sense Task/No Sense Task/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace No_Sense_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string list = Console.ReadLine();
+            string[] ints = list.Split(",");
+            List<int> ls = new List<int>();
+            foreach (var item in ints)
+            {
+                int intValue = int.TryParse(item, out intValue) ? intValue : 0;
+                ls.Add(intValue);
+            }
+            ls = (List<int>)ls.ThisDoesntMakeAnySense(o => o > 50, () => new List<int>() { 1, 2, 3, 4 });
+            foreach (var item in ls)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
